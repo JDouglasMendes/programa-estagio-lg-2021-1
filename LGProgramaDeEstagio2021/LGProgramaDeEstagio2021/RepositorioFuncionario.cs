4e1ee8eb98bf1ce3f2df4f10c59ad1d8840d66cc
DiegoGuse <diego.guse@lg.com.br>
2021-02-09 11:18:34 -0300

    Criaçao Repositorio

---
+++ LGProgramaDeEstagio2021/LGProgramaDeEstagio2021/RepositorioFuncionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGProgramaDeEstagio2021
+{
+    public class RepositorioFuncionario
+    {
+        private List<Funcionario> listaFuncionario;
+        public RepositorioFuncionario()
+        {
+            listaFuncionario = new List<Funcionario>();
+        }
+        public void Insert(Funcionario funcionario)
+        {
+            listaFuncionario.Add(funcionario);
+        }
+
+    }
+}
