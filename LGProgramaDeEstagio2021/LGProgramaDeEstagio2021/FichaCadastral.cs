3d45ea2d550e172e18fc925c95b868c313dfad18
Halleffy Santos <51497671+sinkingstars@users.noreply.github.com>
2021-02-10 09:45:24 -0300

    ficha cadastral

--- LGProgramaDeEstagio2021/LGProgramaDeEstagio2021/FichaCadastral.cs
+++ LGProgramaDeEstagio2021/LGProgramaDeEstagio2021/FichaCadastral.cs
@@ -8,24 +8,54 @@ namespace LGProgramaDeEstagio2021
 {
     public class FichaCadastral
     {
-        public void CadastraFuncionario(string nome, 
-            int matricula, 
+        public void CadastraFuncionario(string nome,  
             float salarioContratual,
             DateTime dataAdmissao, 
             EnumTipoFuncionario enumTipoFuncionario)
         {
+            if (ValidaNome(nome))
+                throw new ArgumentException();
+
+            if (ValidaSalarioContratual(salarioContratual))
+                throw new ArgumentException();
+
+            if (ValidaDataAdmissao(dataAdmissao))
+                throw new ArgumentException();
 
         }
+
+        private bool ValidaDataAdmissao(DateTime dataAdmissao)
+            => dataAdmissao > DateTime.Now;
+
+        private bool ValidaSalarioContratual(float salarioContratual)
+            => salarioContratual <= 0;
+
+       
+        private bool ValidaNome(string nome)
+            => string.IsNullOrEmpty(nome);
+
         public void CadastraFuncionario(string nome, 
-            int matricula, 
             float salarioContratual, 
             DateTime dataAdmissao, 
             EnumTipoFuncionario enumTipoFuncionario,
             CNH cnh)
         {
+            if (ValidaNome(nome))
+                throw new ArgumentException();
+
+            if (ValidaSalarioContratual(salarioContratual))
+                throw new ArgumentException();
+
+            if (ValidaDataAdmissao(dataAdmissao))
+                throw new ArgumentException();
+
+            if (ValidaCNH(cnh))
+                throw new ArgumentException();
 
         }
 
-        
+        private bool ValidaCNH(CNH cnh)
+            => !(cnh != null && cnh.Numero > 0 && cnh.DataValidade != default(DateTime));
+       
     }
 }
